0dbf4067d3a3d89d3f05e3718f854212949e664e
Walter Pogantsch <gitwalter@users.noreply.github.com>
2024-05-16 09:51:06 +0200

    bugfixes

--- pdf-rag.py
+++ pdf-rag.py
@@ -1,12 +1,20 @@
 import streamlit as st
-from llama_index.core import StorageContext, load_index_from_storage, download_loader,\
-     VectorStoreIndex, SimpleDirectoryReader, ChatPromptTemplate, Settings
+from llama_index.core import (
+    StorageContext,
+    load_index_from_storage,
+    download_loader,
+    VectorStoreIndex,
+    SimpleDirectoryReader,
+    ChatPromptTemplate,
+    Settings,
+)
 from llama_index.llms.huggingface import HuggingFaceInferenceAPI
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from dotenv import load_dotenv
 import os
 import base64
 import re
+import shutil
 
 # Load environment variables
 load_dotenv()
@@ -25,19 +33,18 @@ Settings.llm = HuggingFaceInferenceAPI(
     max_new_tokens=1024,
     generate_kwargs={"temperature": 0.0},
 )
-Settings.embed_model = HuggingFaceEmbedding(
-    model_name="BAAI/bge-small-en-v1.5"
-)
+Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
 
 # Define the directory for persistent storage and data
 PERSIST_DIR = "./db"
-DATA_DIR = "data"
+DATA_DIR = "./data"
 
-store_directory = ''
+vector_store_directory = ""
 
 # Ensure data directory exists
 os.makedirs(DATA_DIR, exist_ok=True)
 os.makedirs(PERSIST_DIR, exist_ok=True)
+
 
 def displayPDF(file):
     with open(file, "rb") as f:
@@ -45,11 +52,13 @@ def displayPDF(file):
     pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="600" type="application/pdf"></iframe>'
     st.markdown(pdf_display, unsafe_allow_html=True)
 
+
 def remove_special_characters(s):
     # Define a regular expression pattern to match special characters
-    pattern = r'[^a-zA-Z0-9\s]'
+    pattern = r"[^a-zA-Z0-9\s]"
     # Use the sub() function to replace all occurrences of the pattern with an empty string
-    return re.sub(pattern, '', s)
+    return re.sub(pattern, "", s)
+
 
 def remove_special_characters_from_url(url):
     # Split the URL by "/"
@@ -57,29 +66,49 @@ def remove_special_characters_from_url(url):
     # Get the last part of the URL
     last_part = parts[-1]
     # Define a regular expression pattern to match special characters
-    pattern = r'[^a-zA-Z0-9\s]'
+    pattern = r"[^a-zA-Z0-9\s]"
     # Use the sub() function to replace all occurrences of the pattern with an empty string
-    cleaned_last_part = re.sub(pattern, '', last_part)
+    cleaned_last_part = re.sub(pattern, "", last_part)
     return cleaned_last_part
 
-def data_ingestion_pdf(file_name):
-    documents = SimpleDirectoryReader(DATA_DIR).load_data()
-    index = VectorStoreIndex.from_documents(documents)
-    store_directory = PERSIST_DIR + '/' + remove_special_characters(file_name)
-    os.makedirs(store_directory, exist_ok=True)
-    index.storage_context.persist(persist_dir=store_directory)
 
-def data_ingestion_web(url):
+def create_vector_store(documents, persist_dir):
+    l_index = VectorStoreIndex.from_documents(documents)
+    os.makedirs(persist_dir, exist_ok=True)
+    l_index.storage_context.persist(persist_dir=persist_dir)
+    st.session_state.vector_store_directory = persist_dir
+
+
+def data_ingestion_pdf(data_dir, persist_dir):
+    documents = SimpleDirectoryReader(data_dir).load_data()
+    create_vector_store(documents, persist_dir)
+
+
+def data_ingestion_web(url, persist_dir):
     BeautifulSoupWebReader = download_loader("BeautifulSoupWebReader")
     loader = BeautifulSoupWebReader()
     documents = loader.load_data(urls=[url])
-    index = VectorStoreIndex.from_documents(documents)
-    store_directory = PERSIST_DIR + '/' + remove_special_characters_from_url(url)
-    index.storage_context.persist(persist_dir=store_directory)
+    create_vector_store(documents, persist_dir)
 
-def handle_query(query):
-    storage_context = StorageContext.from_defaults(persist_dir=store_directory)
-    index = load_index_from_storage(storage_context)
+
+def display_delete_button(i_selected_store):
+    # Button to delete the loaded vector store
+    if st.button("Delete Loaded Vector Store"):
+        l_store_directory = PERSIST_DIR + "/" + i_selected_store
+        shutil.rmtree(l_store_directory)
+        l_data_directory = DATA_DIR + "/" + i_selected_store
+        try:
+            shutil.rmtree(l_data_directory)
+        except FileNotFoundError:
+            l_data_directory = ''
+            # for webpages no data-file exists
+            
+        st.info("Loaded vector store " + i_selected_store + " deleted.")
+
+
+def handle_query(query, persist_dir):
+    l_storage_context = StorageContext.from_defaults(persist_dir=persist_dir)
+    l_index = load_index_from_storage(l_storage_context)
     chat_text_qa_msgs = [
         (
             "user",
@@ -97,7 +126,7 @@ def handle_query(query):
     ]
     text_qa_template = ChatPromptTemplate.from_messages(chat_text_qa_msgs)
 
-    query_engine = index.as_query_engine(text_qa_template=text_qa_template)
+    query_engine = l_index.as_query_engine(text_qa_template=text_qa_template)
     answer = query_engine.query(query)
 
     if hasattr(answer, "response"):
@@ -117,11 +146,18 @@ st.markdown("start chat ...🚀")
 existing_vector_stores = os.listdir(PERSIST_DIR)
 
 if "messages" not in st.session_state:
-    st.session_state.messages = [{"role": "assistant", "content": "Hello! Upload a PDF or specify a URL and ask me anything about its content."}]
+    st.session_state.messages = [
+        {
+            "role": "assistant",
+            "content": "Hello! Upload a PDF or specify a URL and ask me anything about its content.",
+        }
+    ]
 
 with st.sidebar:
     st.title("Menu:")
-    uploaded_file = st.file_uploader("Upload your PDF Files or enter a URL and Click on the Submit & Process Button")
+    uploaded_file = st.file_uploader(
+        "Upload your PDF Files or enter a URL and Click on the Submit & Process Button"
+    )
 
     url = st.text_input("Enter URL")
 
@@ -131,27 +167,58 @@ with st.sidebar:
     if st.button("Submit & Process"):
         with st.spinner("Processing..."):
             if url:
-                data_ingestion_web(url)
-            
-            if uploaded_file:
-                filepath = "data/" + uploaded_file.name
+                vector_store_directory = (
+                    PERSIST_DIR + "/" + remove_special_characters_from_url(url)
+                )
+                data_ingestion_web(url, vector_store_directory)
+
+            elif uploaded_file:
+               
+                data_directory = (
+                    DATA_DIR + "/" + remove_special_characters(uploaded_file.name)
+                )
+                
+                os.makedirs(data_directory, exist_ok=True)
+                
+                filepath = (
+                    data_directory
+                    + "/"
+                    + uploaded_file.name
+                )
+                
+                
                 with open(filepath, "wb") as f:
                     f.write(uploaded_file.getbuffer())
-                displayPDF(filepath)  # Display the uploaded PDF
-                data_ingestion_pdf(uploaded_file.name)  # Process PDF every time new file is uploaded
+
+
+
+                displayPDF(filepath)
+
+                vector_store_directory = (
+                    PERSIST_DIR + "/" + remove_special_characters(uploaded_file.name)
+                )
+
+                
+
+                data_ingestion_pdf(data_directory, vector_store_directory)
+
             st.success("Done")
 
 if selected_store:
-    # Load selected vector store
-    store_directory = PERSIST_DIR + "/" + selected_store
-    storage_context = StorageContext.from_defaults(persist_dir=store_directory)
-    index = load_index_from_storage(storage_context)
+    display_delete_button(selected_store)
 
-user_prompt = st.chat_input("Ask me anything about the content of the PDF or the Webpage:")
+user_prompt = st.chat_input(
+    "Ask me anything about the content of the PDF or the Webpage:"
+)
 if user_prompt:
-    store_directory = PERSIST_DIR + "/" + selected_store
+    if selected_store:
+        vector_store_directory = PERSIST_DIR + "/" + selected_store
+
+    if not vector_store_directory:
+        vector_store_directory = st.session_state.vector_store_directory
+
     st.session_state.messages.append({"role": "user", "content": user_prompt})
-    response = handle_query(user_prompt)
+    response = handle_query(user_prompt, vector_store_directory)
     st.session_state.messages.append({"role": "assistant", "content": response})
 
 for message in st.session_state.messages:
